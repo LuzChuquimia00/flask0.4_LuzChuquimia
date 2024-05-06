35945f143beb3c8ff75b2c556fe21032ffa58aac
LuzChuquimia <luzchuquimia1680@gmail.com>
2024-05-06 11:59:18 -0300

    ra

--- Musicas/__int__.py
+++ Musicas/__int__.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, render_template
 
 app = Flask(__name__)
 #
@@ -9,3 +9,24 @@ with app.app_context():
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+#@app.route('/bye')
+#def bye():
+#    return 'Goodbye'
+
+#@app route('/favicon.ico')
+#def favicon():
+#    return send_file('static/favicon.ico') 
+
+@app.route('/themes')
+def songs():
+    data_base = db.get_db() #Consigue la base de datos que estaba en db.py#
+    ask = """
+            SELECT name FROM artists
+            ORDER BY name DESC;
+          """
+    
+    result = data_base.execute(ask)
+    list_of_result = result.fetchall()
+    
+    return render_template("songs.html", songs=list_of_result) 
--- Musicas/db.py
+++ Musicas/db.py
@@ -10,5 +10,44 @@ db_name = 'Musicas.sqlite'
 db_file = os.path.join(db_folder,db_name)
 db_sql_file = 'datos.sql'
 
+def dict_factory(cursor, row):
+   """Arma un diccionario con los valores de la fila."""
+   fields = [column[0] for column in cursor.description]
+   return {key: value for key, value in zip(fields, row)}
+
 def get_db():
     if 'db' not in g: 
+        g.db = sqlite3.connect(
+            db_file,
+            detect_types=sqlite3.PARSE_COLNAMES
+        )
+        g.db.row_factory = sqlite3.Row
+    
+    return g.db
+
+def close_db(e=None):
+    db= g.pop('db',None)
+
+
+    if db is not None:
+        db.close()
+
+def init_db():
+    #Asegurar si la instancia de la carpeta existe.
+    try: 
+        os.makedirs(db_folder)
+    except OSError:
+        pass
+    db = get_db()
+    with current_app.open_instance_resource(db_sql_file) as f:
+        db.executescript(f.read().decode('utf8'))
+
+@click.command('init-db')
+def init_db_command():
+    """Limpia la data existente y crea una nueva."""
+    init_db()
+    click.echo('Inicializa la base de datos')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
